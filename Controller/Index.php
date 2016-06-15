2a011951f70d55e7f1386c7fdf62c93a610bcee4
mucean <moceanliu@163.com>
2016-06-15 18:46:41 +0800

    add post access

--- Controller/Index.php
+++ Controller/Index.php
@@ -6,6 +6,12 @@ class Index
 {
     public function get()
     {
-        $this->response->write('Hello, world!');
+        $this->response->write('Hello, world!' . PHP_EOL)
+            ->write($this->request->getUri()->getQuery());
+    }
+
+    public function post()
+    {
+        $this->response->write('post method');
     }
 }
