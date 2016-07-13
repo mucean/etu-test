42484e86735cfeb137b2aa74c982ca65b61a0c7c
mucean <moceanliu@163.com>
2016-07-13 21:16:10 +0800

    修改test.php

--- test.php
+++ test.php
@@ -1,8 +1,23 @@
 <?php
 
-class Test {
-    public function ecHello()
-    {
-        echo 'Hello, etu!', PHP_EOL;
-    }
-}
+require './boot.php';
+
+$weChatAccessToken = new \Lib\SDK\WeChat\AccessToken();
+
+echo $weChatAccessToken->getAccessToken(), PHP_EOL;
+
+$jsSDK = new \Lib\SDK\WeChat\JSSDK();
+
+var_dump($jsSDK->getConfig('http://www.mucean.com'));
+
+/* $weChatShortLink = new \Lib\SDK\WeChat\ShortLink();
+
+echo $weChatShortLink->getShortLink('http://www.mucean.com'), PHP_EOL; */
+
+/* $sinaShortLink = new \Lib\SDK\Sina\ShortLink();
+
+echo $sinaShortLink->getShortLink('http://www.mucean.com'), PHP_EOL; */
+
+$shortLink = new \Lib\SDK\ShortLink();
+
+echo $shortLink->getShortLink('http://www.mucean.com'), PHP_EOL;
