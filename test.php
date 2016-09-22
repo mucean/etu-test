2a2968ad7b2996427313202ca66f4dfa3e7630d9
rogic <rogic@cubee.com>
2016-09-22 18:41:55 +0800

    add test

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 <?php
 
-require './boot.php';
+require __DIR__ . '/boot.php';
 
-$amqp = new \Lib\RabbitMQ();
+$mysql = new \Etu\Service\Sql\Mysql([
+    'dsn' => 'mysql:host=127.0.0.1;dbname=lottery',
+    'user' => 'root',
+    'password' => 'omymysql'
+]);
 
-$amqp->test();
+//$statement = $mysql->execute('SELECT * FROM ? WHERE userId = ?', ['users', 10000000]);
+$statement = $mysql->execute('SELECT * FROM users WHERE userId = ?', [10000000]);
+echo $statement->queryString, PHP_EOL;
+$statement->debugDumpParams();
+//var_dump($statement->fetchAll());
